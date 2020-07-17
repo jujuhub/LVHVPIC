a8b5511e708cd46c9e4fe2c75906db204f1d6ae2
jujuhub <juhe@ucdavis.edu>
2020-07-17 13:19:58 -0700

    Added CAN message reception and moved functions

--- hih6030.c
+++ hih6030.c
@@ -1,194 +1,22 @@
-/**
-  Generated main.c file from MPLAB Code Configurator
-
-  @Company
-    Microchip Technology Inc.
-
-  @File Name
-    main.c
-
-  @Summary
-    This is the generated main.c using PIC24 / dsPIC33 / PIC32MM MCUs.
-
-  @Description
-    This source file provides main entry point for system initialization and application code development.
-    Generation Information :
-        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - 1.155.0-a
-        Device            :  dsPIC33EV256GM102
-    The generated drivers are tested against the following:
-        Compiler          :  XC16 v1.40
-        MPLAB 	          :  MPLAB X v5.25
-*/
-
 /*
-    (c) 2019 Microchip Technology Inc. and its subsidiaries. You may use this
-    software and any derivatives exclusively with Microchip products.
-
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
-    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
-    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
-    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
-
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
-    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
-    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
-    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
-    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-
-    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
-    TERMS.
-*/
-
-/**
-  Section: Included Files
-*/
-#include "mcc_generated_files/system.h"
-#include "mcc_generated_files/mcc.h"
-#include "mcc_generated_files/interrupt_manager.h"
-
-#define HIH6030_RETRY_MAX 255 // define retry count (100)
-#define HIH6030_I2C_TIMEOUT 255 // define slave timeout (50)
-#define HIH6030_ADDRESS 0x27 // HIH6030 slave device address
-
-/*
- *          Local functions
+ * File: hih6030.c
+ * Author: Julie He <juhe@ucdavis.edu>
+ * 
+ * Description: This script contains functions used to read and write 
+ * data to the Honeywell HumidIcon relative humidity and temperature 
+ * sensor, HIH6030-021. 
+ * 
+ * Created on July 17, 2020, 11:55 AM
  */
-void HIH6030_Write(uint8_t command, uint16_t dat, I2C1_MESSAGE_STATUS *pstatus);
-void HIH6030_Read(uint8_t command, uint8_t *pData);
-uint8_t fetch_RHT(float *pHum, float *pTemp);
 
-/*
-                         Main application
- */
-int main(void)
-{
-    // initialize the device
-    SYSTEM_Initialize();
-    INTERRUPT_GlobalEnable();
-    
-    uint8_t d = 0;
-    for(d = 0; d < 255; ++d); // some delay
-    
-    /*
-     *      HIH6030 Relative Humidity & Temperature Sensor
-     */
-    
-    /* Enter Command Mode within 3 ms */
-//    I2C1_MESSAGE_STATUS i2cStatus;
-//    HIH6030_Write(0xA0, 0x0000, &i2cStatus);
-//    while (i2cStatus != I2C1_MESSAGE_COMPLETE);
+#include <stdio.h>
+#include <stdlib.h>
 
-    /* Read/Write from EEPROM location */
-//    uint8_t cmdmodeData[3], *pD;
-//    pD = cmdmodeData;
+#include "hih6030.h"
 
-//    HIH6030_Read(0x18, pD); // Alarm_High_On
-
-//    i2cStatus = I2C1_MESSAGE_PENDING; // need to reset msg status?
-//    HIH6030_Write(0x58, 0x3333, &i2cStatus); // set Alarm_High_On at 80% RH
-//    HIH6030_Write(0x59, 0x3000, &i2cStatus); // set Alarm_High_Off at 75% RH
-//    HIH6030_Write(0x5A, 0x0CCD, &i2cStatus); // set Alarm_Low_On at 20% RH
-//    HIH6030_Write(0x5B, 0x1000, &i2cStatus); // set Alarm_Low_Off at 25% RH
-
-    /* Enter Normal Operation */
-//    HIH6030_Write(0x80, 0x0000, &i2cStatus);
-
-    /* Make a humidity and temperature measurement, fetch the results */
-    float hum = 0., temp = 0., *pHum, *pTemp;
-    pHum = &hum;
-    pTemp = &temp;
-    uint8_t _status = fetch_RHT(pHum, pTemp);
-    
-    switch(_status) // should use this to send CAN messages to Raspberry Pi
-    {
-        case 0:
-            printf("Normal.\n");
-            // send data to RP
-            break;
-        
-        case 1:
-            printf("Stale Data.\n");
-            // do not send data; try to make a new measurement
-            break;
-        
-        case 2:
-            // this shouldn't happen in Normal Operation mode
-            printf("Command Mode.\n");
-            // send warning to RP
-            break;
-        
-        default: 
-            printf("Diagnostic, or Invalid Data.\n");
-            // send warning to RP
-            break;
-    } // end RH&T sensor
-
-
-    /* CANbus testing */
-//    C1TR01CONbits.TXEN0 = 0x1; // already done in can1.c
-//    C1TR01CONbits.TX0PRI = 0x3;
-    
-//    can1msgBuf[0][0] = 0x123;
-
-    while (1)
-    {
-        // Add your application code
-        
-        /* Make RH&T measurement, fetch results from HIH6030-021 sensor */
-        
-        
-        /* CANbus testing */
-        // transmission
-        uCAN_MSG txCANmsg, *pTxCANmsg;
-        pTxCANmsg = &txCANmsg;
-        
-        txCANmsg.frame.id = 0x123;
-        txCANmsg.frame.idType = CAN_FRAME_STD;
-        txCANmsg.frame.msgtype = CAN_MSG_DATA;
-        txCANmsg.frame.dlc = 0b1000;
-        txCANmsg.frame.data0 = 0xDE;
-        txCANmsg.frame.data1 = 0xAD;
-        txCANmsg.frame.data2 = 0xBE;
-        txCANmsg.frame.data3 = 0xEF;
-        txCANmsg.frame.data4 = 0x00;
-        txCANmsg.frame.data5 = 0x00;
-        txCANmsg.frame.data6 = 0x00;
-        txCANmsg.frame.data7 = 0x00;
-        
-        CAN1_TransmitEnable();
-        
-        CAN_TX_PRIOIRTY msg_prio = CAN_PRIORITY_MEDIUM;
-        CAN1_transmit(msg_prio, pCANmsg);
-        while (C1TR01CONbits.TXREQ0 == 1);
-
-        // reception
-        uCAN_MSG rxCANmsg, *pRxCANmsg;
-        pRxCANmsg = &rxCANmsg;
-        uint32_t msgID;
-        
-        CAN1_ReceiveEnable();
-        
-        CAN1_receive(pRxCANmsg);
-        while (C1RXFUL1 == 0x0000)
-        {
-            if (C1RXFUL1bits.RXFUL1 == 0) // if specific register is empty
-                break;
-        }
-        C1RXFUL1bits.RXFUL1 = 0;
-        
-        msgID = rxCANmsg.frame.id;
-        
-        switch(msgID)
-        {
-            case (0x123):
-                
-        }
-    }
-    return 1; 
-}
+#define HIH6030_ADDR 0x27
+#define HIH6030_MAX_RETRY 255
+#define HIH6030_I2C_TIMEOUT 255
 
 uint8_t fetch_RHT(float *pHum, float *pTemp)
 {
@@ -205,7 +33,7 @@ uint8_t fetch_RHT(float *pHum, float *pTemp)
     uint8_t writeBuffer[1], sensorData[4] = {0}, _status, *pD;
     uint16_t H_dat, T_dat, retryTimeOut = 0, slaveTimeOut = 0;
     pD = sensorData;
-    writeBuffer[0] = (HIH6030_ADDRESS << 1); // dummy data
+    writeBuffer[0] = (HIH6030_ADDR << 1); // dummy data
 
     I2C1_MESSAGE_STATUS i2cStatus;
     i2cStatus = I2C1_MESSAGE_PENDING;
@@ -217,7 +45,7 @@ uint8_t fetch_RHT(float *pHum, float *pTemp)
     /* Send Measurement Request (MR) */
     while (i2cStatus != I2C1_MESSAGE_COMPLETE)
     {
-        I2C1_MasterWrite(writeBuffer, 1, HIH6030_ADDRESS, &i2cStatus);
+        I2C1_MasterWrite(writeBuffer, 1, HIH6030_ADDR, &i2cStatus);
         
         while(i2cStatus == I2C1_MESSAGE_PENDING)
         {
@@ -229,7 +57,7 @@ uint8_t fetch_RHT(float *pHum, float *pTemp)
         }
         
         // check for max retry
-        if (retryTimeOut == HIH6030_RETRY_MAX)
+        if (retryTimeOut == HIH6030_MAX_RETRY)
             break;
         else
             retryTimeOut++;
@@ -248,7 +76,7 @@ uint8_t fetch_RHT(float *pHum, float *pTemp)
         
         while (i2cStatus != I2C1_MESSAGE_FAIL)
         {
-            I2C1_MasterRead(pD, 4, HIH6030_ADDRESS, &i2cStatus);
+            I2C1_MasterRead(pD, 4, HIH6030_ADDR, &i2cStatus);
             
 //            uint8_t dt;
 //            for (dt = 0; dt < 255; ++dt);
@@ -266,7 +94,7 @@ uint8_t fetch_RHT(float *pHum, float *pTemp)
             }
 
             // check for max retry
-            if (retryTimeOut == HIH6030_RETRY_MAX)
+            if (retryTimeOut == HIH6030_MAX_RETRY)
                 break;
             else
                 retryTimeOut++;
@@ -288,7 +116,7 @@ uint8_t fetch_RHT(float *pHum, float *pTemp)
         
         while (i2cStatus != I2C1_MESSAGE_FAIL)
         {
-            I2C1_MasterRead(pRD, 4, HIH6030_ADDRESS, &i2cStatus);
+            I2C1_MasterRead(pRD, 4, HIH6030_ADDR, &i2cStatus);
 
             while (i2cStatus == I2C1_MESSAGE_PENDING)
             {
@@ -302,7 +130,7 @@ uint8_t fetch_RHT(float *pHum, float *pTemp)
             }
 
             // check for max retry
-            if (retryTimeOut == HIH6030_RETRY_MAX)
+            if (retryTimeOut == HIH6030_MAX_RETRY)
                 break;
             else
                 retryTimeOut++;
@@ -359,7 +187,7 @@ void HIH6030_Write(uint8_t command, uint16_t dat, I2C1_MESSAGE_STATUS *pstatus)
     while (*pstatus != I2C1_MESSAGE_FAIL)
     {
         // supposedly this only writes 1 byte? #TODO: CHECK THIS!
-        I2C1_MasterWrite(writeBuffer, 3, HIH6030_ADDRESS, pstatus);
+        I2C1_MasterWrite(writeBuffer, 3, HIH6030_ADDR, pstatus);
         
         // wait for msg to be sent or status changed
         while (*pstatus == I2C1_MESSAGE_PENDING)
@@ -379,7 +207,7 @@ void HIH6030_Write(uint8_t command, uint16_t dat, I2C1_MESSAGE_STATUS *pstatus)
             break;
         
         // check for max retry and skip this byte
-        if (retryTimeOut == HIH6030_RETRY_MAX)
+        if (retryTimeOut == HIH6030_MAX_RETRY)
             break;
         else
             retryTimeOut++;
@@ -427,7 +255,7 @@ void HIH6030_Read(uint8_t command, uint8_t *pData)
         
             while (i2cStatus != I2C1_MESSAGE_FAIL)
             {
-                I2C1_MasterRead(pD, 1, HIH6030_ADDRESS, &i2cStatus);
+                I2C1_MasterRead(pD, 1, HIH6030_ADDR, &i2cStatus);
             
                 while (i2cStatus == I2C1_MESSAGE_PENDING)
                 {
@@ -446,7 +274,7 @@ void HIH6030_Read(uint8_t command, uint8_t *pData)
                     break;
                 
                 // check for retry and skip this byte
-                if (retryTimeOut == HIH6030_RETRY_MAX)
+                if (retryTimeOut == HIH6030_MAX_RETRY)
                     break;
                 else
                     retryTimeOut++;
@@ -465,8 +293,3 @@ void HIH6030_Read(uint8_t command, uint8_t *pData)
     
     //return;
 }
-
-/**
- End of File
-*/
-
---
+++ hih6030.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   hih6030.h
+ * Author: physicslaptop
+ *
+ * Created on July 17, 2020, 11:56 AM
+ */
+
+#ifndef HIH6030_H
+#define	HIH6030_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HIH6030_H */
+
